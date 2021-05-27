77f3e442c54c5a08fa95ebbc65a2a1c7566b34cb
chul0721 <chul070721@naver.com>
2021-05-27 18:30:28 +0900

    fix: Fixed bugs

--- src/api/Router.ts
+++ src/api/Router.ts
@@ -1,7 +1,12 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 const router = Router()
 
-router.get('/', () => console.log('개발중임'))
+router.get('/', (req: Request, res: Response) => {
+  res.send('asdf')
+  console.log('===========================')
+  console.log(`어떤 놈이 접속했음: IP: ${req.ip}`)
+  console.log('===========================')
+})
 
 export default router
