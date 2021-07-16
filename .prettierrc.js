a34c8d53a49cf026d4e86482100523c5c4338433
chul0721 <chul070721@naver.com>
2021-07-16 15:59:09 +0900

    style: code format

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: false,
-  trailingComma: 'none',
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false
-}
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,3 @@
 import main from './main'
 
-export {
-    main
-}
+export { main }
