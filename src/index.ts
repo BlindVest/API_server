014f57220582e4103fce384553faa3ab75800a50
chul0721 <chul070721@naver.com>
2021-07-18 18:01:42 +0900

    fix: fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import router from './router'
 
 const app: express.Application = express()
 dotenv.config({ path: '../.env' })
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 const corsOptions: object = {
   origin: '*'
 }
@@ -14,11 +14,11 @@ const urlencodedOptions: object = {
   extended: true
 }
 
-app.use(router)
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded(urlencodedOptions))
 app.set('trust proxy', true)
+app.use(router)
 
 app.listen(port, () => {
   console.log('\x1b[36m%s\x1b[34m%s\x1b[0m', '[Server]', ` Server on  : ${port}`)
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,6 @@ import { main } from './api'
 
 const router = Router()
 
-router.get('/', main)
+router.post('/', main)
 
 export default router
